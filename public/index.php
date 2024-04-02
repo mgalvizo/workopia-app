908736af3c82aa8c5794043e1d7de5b0caefd605
Miguel Garcia <miguelangelgarciaalvizo@hotmail.com>
2024-04-01 20:08:21 -0600

    create basic router

--- public/index.php
+++ public/index.php
@@ -2,5 +2,22 @@
 <?php
 // Helpers is available for the whole app
 require '../helpers.php';
-loadView('home');
+
+// Basic Routing
+$routes = [
+    '/workopia-app/public/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php',
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+// inspectAndDie(($uri));
+
+// array_key_exists checks if the given key or index exists in the array
+if (array_key_exists($uri, $routes)) {
+    require(basePath($routes[$uri]));
+} else {
+    require basePath($routes['404']);
+}
 ?>
