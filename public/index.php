8a8236a44f6e642377742481d1bd7d7fe114c009
Miguel Garcia <miguelangelgarciaalvizo@hotmail.com>
2024-04-01 22:59:15 -0600

    refactor router to class, implement in index entry point

--- public/index.php
+++ public/index.php
@@ -3,21 +3,19 @@
 // Helpers is available for the whole app
 require '../helpers.php';
 
-// Basic Routing
-$routes = [
-    '/' => 'controllers/home.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php',
-];
+require basePath('Router.php');
+
+// Instantiate router
+$router = new Router();
+
+// Require routes file
+$routes = require basePath('routes.php');
 
 $uri = $_SERVER['REQUEST_URI'];
 // inspectAndDie(($uri));
 
-// array_key_exists checks if the given key or index exists in the array
-if (array_key_exists($uri, $routes)) {
-    require(basePath($routes[$uri]));
-} else {
-    require basePath($routes['404']);
-}
+$method = $_SERVER['REQUEST_METHOD'];
+// inspectAndDie(($method));
+
+$router->route($uri, $method);
 ?>
