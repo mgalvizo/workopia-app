c3874e814e2a157cf17f8037af3996ea1c3c80ec
Miguel Garcia <miguelangelgarciaalvizo@hotmail.com>
2024-04-01 18:27:21 -0600

    split ui into partials, refactor helper functions

--- helpers.php
+++ helpers.php
@@ -5,7 +5,43 @@
  * @return string
  */
 function basePath($path = '') {
-    // to return the absolute path
-    return __DIR__ . '/' . $path;
+  // to return the absolute path
+  return __DIR__ . '/' . $path;
+}
+
+/**
+ * Load a view
+ * @param string $name
+ * @return void
+ * 
+ */
+function loadView($name) {
+  // Check if the view exists
+  $viewPath = basePath("views/{$name}.view.php");
+  
+  // file_exists checks whether a file or directory exists
+  if (file_exists($viewPath)) {
+      require $viewPath;
+  } else {
+      echo "View '{$name}' not found!";
+  }
+}
+
+/**
+ * Load a partial
+ * @param string $name
+ * @return void
+ * 
+ */
+function loadPartial($name) {
+
+  $partialPath = basePath("views/partials/{$name}.php");
+
+  // file_exists checks whether a file or directory exists
+  if (file_exists($partialPath)) {
+      require $partialPath;
+  } else {
+      echo "Partial '{$name}' not found!";
+  }
 }
 ?>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <!-- Make sure the configuration of your auto.workopia-app.test.conf includes the public folder as Document Root -->
 <?php
+// Helpers is available for the whole app
 require '../helpers.php';
-require basePath('views/home.view.php');
+loadView('home');
 ?>
