010bf25e6d65848a63fbe6c1a69ef079e50e3f1a
Miguel Garcia <miguelangelgarciaalvizo@hotmail.com>
2024-04-03 11:06:54 -0600

    add login and register routes, create user controller and register and login views

--- routes.php
+++ routes.php
@@ -12,6 +12,12 @@ $router->post('/listings', 'ListingController@store');
 $router->put('/listings/{id}', 'ListingController@update');
 $router->delete('/listings/{id}', 'ListingController@destroy');
 
+// Auth
+$router->get('/auth/register', 'UserController@create');
+$router->get('/auth/login', 'UserController@login');
+
+
+
 // $router->get('/', 'controllers/home.php');
 // $router->get('/listings', 'controllers/listings/index.php');
 // $router->get('/listings/create', 'controllers/listings/create.php');
